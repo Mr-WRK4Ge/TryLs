667411f40483b49bbafdbc6e929dfd689e2fde65
Mr WRK4Ge <73357233+Mr-WRK4Ge@users.noreply.github.com>
2020-12-01 23:37:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'TryLs_9z BoT'; 
+const BotName = 'TryLs_9z'; 
 const instagram = 'https://instagram/tryls3';  
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
 //const grupch1 = 'belum ada grup'; 
@@ -62,7 +62,7 @@ fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json')
 //conn.connectOptions.agent = ProxyAgent ('http://1.0.180.120:8080')
 conn.connect();
 
-conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@reuz.id')
+conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@tryls3.id')
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
